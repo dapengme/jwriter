ae89ecd9e9cc8e45dbdea26f7574914970a80595
zhaopeng <imzhpe@qq.com>
2013-05-16 18:56:46 +0800

    加入替换beetl模板引擎

--- src/main/java/com/beardnote/jwriter/web/front/LoginController.java
+++ src/main/java/com/beardnote/jwriter/web/front/LoginController.java
@@ -12,17 +12,18 @@ import com.beardnote.jwriter.model.User;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String loginForm(HttpServletRequest request) {
-		User user = new User();
-		user.setUsername("zhaopeng");
-		request.getSession().setAttribute(Constants.Front.USER_KEY, user);
-//		return "redirect:/app/frontPage/CACHEY,frontPage,1,1";
-		return "redirect:/";
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginForm(HttpServletRequest request) {
+        User user = new User();
+        user.setUsername("zhaopeng");
+        request.getSession().setAttribute(Constants.Front.USER_KEY, user);
+        // return "redirect:/app/frontPage/CACHEY,frontPage,1,1";
+        return "redirect:/";
+    }
 
 }
